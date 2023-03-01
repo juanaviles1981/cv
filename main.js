068f2befbae2cecac4359aee823d94289e4bca6f
Juan Aviles <111673805+juanaviles1981@users.noreply.github.com>
2023-03-01 03:22:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+document.getElementById('experiencia_mas').addEventListener('click', function(){
+    document.getElementById('detalle_exp').style.display = 'block';
+})
+
+document.getElementById('experiencia_menos').addEventListener('click', function(){
+    document.getElementById('detalle_exp').style.display = 'none';
+})
+
+
+document.getElementById('habilidades_mas').addEventListener('click', function(){
+    document.getElementById('detalle_hab').style.display = 'block';
+})
+
+document.getElementById('habilidades_menos').addEventListener('click', function(){
+    document.getElementById('detalle_hab').style.display = 'none';
+})
+
+document.getElementById('intereses_mas').addEventListener('click', function(){
+    document.getElementById('detalle_int').style.display = 'block';
+})
+
+document.getElementById('intereses_menos').addEventListener('click', function(){
+    document.getElementById('detalle_int').style.display = 'none';
+})
